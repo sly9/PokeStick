2587764fd5655a3d0a06e558c24a12cabf3b2feb
sly9 <sly.thu@gmail.com>
2016-07-16 10:59:36 -0700

    relative location.

--- updateLocation.php
+++ updateLocation.php
@@ -6,9 +6,9 @@ $lng = $_REQUEST["lng"];
 $xmlString = '<gpx><wpt lat="'.$lat.'" lon="'.$lng.'"></wpt></gpx>';
 echo $xmlString;
 file_put_contents('location.gpx',$xmlString);
-shell_exec('/Users/liuyisun/tmp/PokeStick/autoclicker_bin 547 1073');
+shell_exec('./autoclicker_bin 547 1073');
 sleep(0.1);
-shell_exec('/Users/liuyisun/tmp/PokeStick/autoclicker_bin 547 1115');
+shell_exec('./autoclicker_bin 547 1115');
 //547 1315
 //547 1359
 ?>
